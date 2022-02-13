39010f7e6d1d87a7e7227daf4617752c3fbbf217
shouvikbasu <babanomania@gmail.com>
2022-02-13 23:54:25 +0530

    logo update

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,13 +3,28 @@ import { Helmet } from "react-helmet";
 import config from "../config.json";
 
 export default function Layout({ children }) {
+  const Logo = () => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-6 w-6"
+      viewBox="0 0 20 20"
+      fill="currentColor"
+    >
+      <path d="M5 4a2 2 0 012-2h6a2 2 0 012 2v14l-5-2.5L5 18V4z" />
+    </svg>
+  );
+
   return (
     <div className="w-screen min-h-screen dark">
       <Helmet>
         <meta charSet="utf-8" />
         <title>{config.name}</title>
       </Helmet>
-      <header>{config.name}</header>
+      <header>
+        <span className="flex flex-row space-x-2">
+          <Logo /> <span>{config.name}</span>
+        </span>
+      </header>
       <main>{children}</main>
     </div>
   );
